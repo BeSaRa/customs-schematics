245fa774cbfe78db26c5acc639c1ded5985d64a6
BeSaRa <ahmedbesara@gmailcom>
2023-05-02 10:38:51 +0300

    finish: create-admin-popup-component

--- src/create-admin-popup-component/index.ts
+++ src/create-admin-popup-component/index.ts
@@ -1,10 +1,27 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  apply,
+  applyTemplates,
+  chain, mergeWith,
+  move,
+  Rule,
+  SchematicContext,
+  strings,
+  Tree,
+  url
+} from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function createAdminPopupComponent(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export function createAdminPopupComponent(_options: {name: string}): Rule {
+  return (_tree: Tree, _context: SchematicContext) => {
+    const templateSource = apply(url('./files'), [
+      applyTemplates({
+        ..._options,
+        ...strings
+      }),
+      move('.')
+    ])
+    return chain([mergeWith(templateSource)]);
   };
 }
