2aa6c9d913acab1fc8f5cdf38c3b838918e9a464
BeSaRa <ahmedbesara@gmailcom>
2023-05-03 05:48:02 +0300

    update the schematics file

--- src/create-admin-service/index.ts
+++ src/create-admin-service/index.ts
@@ -1,12 +1,12 @@
 import {Rule, schematic, SchematicContext, Tree} from '@angular-devkit/schematics';
-import {AppSchematics} from "../shematics";
+import {AppSchematics} from "../schematics";
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function createAdminService(_options: { name: string, url: string, parent: string, debugMode: boolean }): Rule {
     return (_tree: Tree, _context: SchematicContext) => {
-        const rules = AppSchematics.map((item) => {
+        const rules = AppSchematics.map((item: string) => {
             return schematic(item, _options, {
                 interactive: false
             })
